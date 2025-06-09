59ee3aad7757d1ea74a894130e458312a654c039
christianfelipecarvalho <christianfc001@gmail.com>
2025-06-09 00:12:57 -0300

    Add link PRD

--- Tarefando/Program.cs
+++ Tarefando/Program.cs
@@ -20,8 +20,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new() { Title = "Tarefando API", Version = "v1" });
-    c.AddServer(new() { Url = "http://localhost:8080", Description = "Development Server" });
-    c.AddServer(new() { Url = "http://localhost:8080", Description = "Production Server" });
+    //c.AddServer(new() { Url = "http://localhost:8080", Description = "Development Server" });
+    c.AddServer(new() { Url = "https://tarefando.onrender.com", Description = "Development Server" });
+    c.AddServer(new() { Url = "https://tarefando.onrender.com", Description = "Production Server" });
 });
 
 builder.Services.AddInfrastructureServices(builder.Configuration);
